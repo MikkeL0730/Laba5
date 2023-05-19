005f6752fd56949105bef6cb0ed1efdce186f0c5
MikkeL0730 <57572232+MikkeL0730@users.noreply.github.com>
2023-05-19 13:22:41 +0400

    Add files via upload

--- laba5.py
+++ laba5.py
@@ -1,77 +1,61 @@
-def F_rec(n):
-    if n == 1:
-        return 1
-    else:
-        return F_rec(n-1) * (2*n-1)
-def F_it(n):
-    f_n = [1]*(n+1)
-    for i in range(3, n+1):
-        f_n[i] = f_n[i-2] * (i-1)
-    return f_n[n]
-
 import time
 import matplotlib.pyplot as plt
 
-recursive_times = []
-iterative_times = []
-try:
-    print("Введите натуральное число n:")
-    n = int(input())
-    while n < 1:
-        n = int(input("\nВы указали не натуральное число. Введите натуральное число n:"))
+def rek (n):
+    if n == 1:
+        return 1
+    if n > 1:
+        return rek(n - 1) * (2 * n - 1)
 
-    print("Введите шаг изменения s (натуральное число):")
+def iter (n):
+    k1 = 1
+    if n == 1:
+        return 1
+    if n > 1:
+        for i in range(2, n + 1):
+            k1 = k1 * (2 * i - 1)
+        return k1
 
-    s = int(input())
-    while s < 1:
-        s = int(input("\nВы указали не натуральное число. Введите шаг изменения s (натуральное число):"))
+while True:
+    n = input('введите число n: ')
+    if n.isdigit() == True:
+        n = int(n)
+        break
+
+while True:
+    wag = input('Введите шаг изменения числа: ')
+    if wag.isdigit() == True:
+        wag = int(wag)
+        break
+n_values = list(range(1, n + 1, wag))
+
+iter_times = []
+iter_values = []
+rek_times = []
+rek_values = []
+table = []
+
+for i in n_values:
+    start = time.perf_counter()
+    iter_values.append(iter(i))
+    all_time = time.perf_counter() - start
+    iter_times.append(all_time)
+    start = time.perf_counter()
+    rek_values.append(iter(i))
+    all_time = time.perf_counter() - start
+    rek_times.append(all_time)
+
+for i, n in enumerate(n_values):
+    table.append([n, rek_times[i], iter_times[i], rek_values[i], iter_values[i]])
+print('{:<7}|{:<22}|{:<22}|{:<22}|{:<22}'.format('n', 'Время рекурсии (с)', 'Время итерации (с)','Значение рекурсии', 'Значение итерации'))
+print('-' * 110)
+for data in table:
+    print('{:<7}|{:<22}|{:<22}|{:<22}|{:<22}'.format(data[0], data[1], data[2], data[3], data[4]))
 
 
-    start_time = time.time()
-    result = F_it(n)
-    print("\nРезультат итерации -", result, "\nВремя работы -", time.time() - start_time)
-    start_time = time.time()
-    result = F_rec(n)
-    print("\nРезультат рекурсии -", result, "\nВремя работы -", time.time() - start_time)
-
-
-    rec_times = []
-    rec_values = []
-    it_times = []
-    it_values = []
-    n_values = list(range(1, n+1, s))
-
-    for n in n_values:
-        start_time = time.time()
-        rec_values.append(F_rec(n))
-        end_time = time.time()
-        result_time = end_time - start_time
-        rec_times.append(result_time)
-        print(start_time, end_time)
-
-        start_time = time.time()
-        it_values.append(F_it(n))
-        end_time = time.time() - start_time
-        it_times.append(end_time)
-
-    t_data = []
-    for i, n in enumerate(n_values):
-        t_data.append([n, rec_times[i],rec_values[i],it_times[i],it_values[i]])
-    print('{:<5}|{:<20}|{:<20}|{:<20}|{:<20}'.format('n', 'Время рекурсии', 'Значение рекурсии', 'Время итерации', 'Значение итерации'))
-    for data in t_data:
-        print('{:<5}|{:<20}|{:<20}|{:<20}|{:<20}'.format(data[0], data[1], data[2], data[3], data[4]))
-    #'''
-    plt.plot(n_values, rec_times, label="Recursive")
-    plt.plot(n_values, it_times, label="Iterative")
-    plt.legend()
-    plt.show()
-    #'''
-
-    print(time.time())
-
-except ValueError:
-    print("\nВы ввели не натуральное число. Перезапустите программу.")
-
-except RecursionError:
-    print(
-        "\nВы ввели слишком большое число. Перезапустите программу.")
+plt.plot(n_values, iter_times, label='итерация')
+#plt.plot(n_values, rek_times, label='рекурсия')
+plt.xlabel('n')
+plt.ylabel('Время (с)')
+plt.legend()
+plt.show()
